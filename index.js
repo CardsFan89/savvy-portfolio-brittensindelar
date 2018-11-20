5efc7c0b0df80456bb50ad4f42c9cba424af830e
Britten Sindelar <brittensindelar@gmail.com>
2018-11-19 21:04:49 -0600

    added javascript

---
+++ index.js
@@ -0,0 +1,15 @@
+var userName = prompt('What is  your name?');
+
+var greet = function greet(){
+    if(userName){
+        userName = prompt( 'What is your name, for the this time?');
+    
+greet();
+
+    }
+else{ 
+    alert ('Hello ' + userName);
+
+ };};
+
+greet();
