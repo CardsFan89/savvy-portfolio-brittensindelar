a7d6fec6d9e5bb6a64d2a309f09e09596eb17587
Britten Sindelar <brittensindelar@gmail.com>
2018-12-03 18:47:53 -0600

    update index.js

--- src/header.js
+++ src/header.js
@@ -1,15 +1,12 @@
 export default function Header(state){
-    console.log(state);
-    console.log(state[state.active]);
-    
-return `
-<div id="header">
-<div class="container">
-    <h1>${state[state.active].title}</h1>
-    <h2>My name is Britten Sindelar</h2>
-    </div>
-</div>
+    return `
+         <div id="header">
+            <div class="container">
+                <h1>${state[state.active].title}</h1>
+                <h2>My name is Britten Sindelar</h2>
+            </div>
+</div >
 
-`;
+        `;
 }
 
